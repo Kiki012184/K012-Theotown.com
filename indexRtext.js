5378a8b55022e4da4723d1441ba91aef0e285c98
Kiki012 <62149576+Kiki012184@users.noreply.github.com>
2022-07-23 13:18:43 +0700

    Delete indexRtext.js

--- indexRtext.js
+++
@@ -1,5 +0,0 @@
-var r_text = new Array ();
-r_text[0] = "<p>Hello there! :)</p>";
-r_text[1] = "<p>This website is work in progress!</p>";
-var i = Math.floor(r_text.length * Math.random());
-document.write(r_text[i]);
